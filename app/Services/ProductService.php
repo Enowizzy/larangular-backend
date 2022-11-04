6744e541255b45b34388978ce2fd234d5e31ae8f
Jeedie JIDE <enockyona906@gmail.com>
2022-11-04 19:43:05 +0300

    perform changes

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -17,15 +17,19 @@ class ProductService
     {
 
         $file = [];
-        if ($request->hasFile('images')) {
-            foreach ($request->file('images') as $file) {
+        if ($request->hasFile('image')) {
+            $file = $request->hasFile('image');
+            foreach ($request->file('image') as $file) {
                 $destination_path = 'public/products';
                 $fileName = $file->getClientOriginalExtension();
                 $filename = time() . '-' . md5(rand(1000, 10000)) . '.' . $fileName;
                 $path = $file->storeAs($destination_path,  $filename);
                 $image[] = $filename;
+
+
             }
         }
+       
         $input = $request->validated();
         $json = Product::create(array_merge(
             $input,
